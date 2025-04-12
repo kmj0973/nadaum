9e84f273bb0e0578c28d573efa6120fe37d3e738
kmj0973 <kmj0973@naver.com>
2025-04-13 01:33:08 +0900

    Feat: home page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+import photomap from "../../public/images/photomap.png";
+
 export default function HomePage() {
   return (
     <div className="w-full min-h-[750px] h-[100vh] flex flex-col items-center scroll-auto">
       <div className="bg-white w-full flex flex-col items-center gap-5 py-5">
         <div className="relative rounded-xl shadow-md w-[90%] h-[166px] tablet:h-[186px]">
-          <div className="text-lg font-bold mt-5 ml-5">
+          <div className="text-[16px] tablet:text-lg font-bold mt-5 ml-5">
             내 근처 공공시설 찾기
           </div>
           <div className="absolute bottom-3 right-5 text-[13px] text-[#767676] ">
@@ -37,6 +40,47 @@ export default function HomePage() {
             <div className="text-[13px] tablet:text-base text-[#767676] font-normal ml-5">
               운동이 쉬워지는 첫걸음
             </div>
+          </div>
+        </div>
+        <div className="bg-[#18B491] rounded-xl shadow-md w-[90%] h-[564px] tablet:h-[670px] flex flex-col items-center justify-center gap-5">
+          <div>
+            <div className="text-white text-2xl font-semibold text-center mb-2">
+              근처 체육시설을
+              <br />
+              간편하게 찾아보세요
+            </div>
+            <div className="text-[#EEEEEE] text-[13px] text-center">
+              카카오맵 지도 연동 시스템을 사용하여
+              <br />
+              쉽게 찾을 수 있습니다
+            </div>
+          </div>
+
+          <div className="w-full max-w-[240px]">
+            <Image
+              src={photomap}
+              alt="photomap"
+              sizes="100vw"
+              // Make the image display full width
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+          </div>
+          <div className="w-full flex flex-col justify-center items-center">
+            <div className="text-white mb-2">원하는 지역을 찾아보세요</div>
+            <button className="bg-white/15 text-white border-1 border-white rounded-2xl w-[120px] h-[36px]">
+              검색하기
+            </button>
+          </div>
+        </div>
+        <div className="w-[90%] flex justify-between items-center">
+          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[220px] p-2">
+            출석체크
+          </div>
+          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[220px] p-2">
+            운동기록
           </div>
         </div>
       </div>
