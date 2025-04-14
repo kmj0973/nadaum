fba12c8764d0c4ea5d0475a7872f5352193b34fd
kmj0973 <kmj0973@naver.com>
2025-04-14 14:43:24 +0900

    Feat: footer navbar components

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,99 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-white fixed bottom-0 w-full max-w-[500px] h-[60px] drop-shadow-xl flex justify-around items-center ">
+      <div className="flex flex-col justify-center items-center gap-0.5 flex-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M1.71484 14.0572L12.022 4.62866L22.2863 14.0187"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M5.14355 11.4858V21.7716H18.8578V11.4858"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <div className="text-center text-[12px]">홈</div>
+      </div>
+      <div className="flex flex-col justify-center items-center flex-1 mt-0.5 gap-0.5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M21.5996 10C21.5996 17 12.5996 23 12.5996 23C12.5996 23 3.59961 17 3.59961 10C3.59961 7.61305 4.54782 5.32387 6.23565 3.63604C7.92348 1.94821 10.2127 1 12.5996 1C14.9866 1 17.2757 1.94821 18.9636 3.63604C20.6514 5.32387 21.5996 7.61305 21.5996 10Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12.5996 13C14.2565 13 15.5996 11.6569 15.5996 10C15.5996 8.34315 14.2565 7 12.5996 7C10.9428 7 9.59961 8.34315 9.59961 10C9.59961 11.6569 10.9428 13 12.5996 13Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <div className="text-center text-[12px]">공공체육시설</div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-0.5 flex-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M21 11.5C21.0034 12.8199 20.695 14.1219 20.1 15.3C19.3944 16.7118 18.3097 17.8992 16.9674 18.7293C15.6251 19.5594 14.0782 19.9994 12.5 20C11.1801 20.0034 9.8781 19.6951 8.69999 19.1L3 21L4.9 15.3C4.30493 14.1219 3.99656 12.8199 4 11.5C4.00061 9.92177 4.44061 8.37487 5.27072 7.03257C6.10082 5.69027 7.28825 4.60559 8.69999 3.90003C9.8781 3.30496 11.1801 2.99659 12.5 3.00003H13C15.0843 3.11502 17.053 3.99479 18.5291 5.47088C20.0052 6.94698 20.885 8.91567 21 11V11.5Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <div className="text-center text-[12px]">AI 챗봇</div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-0.5 flex-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M11.9996 11.9999C14.3665 11.9999 16.2853 10.0811 16.2853 7.71418C16.2853 5.34725 14.3665 3.42847 11.9996 3.42847C9.63265 3.42847 7.71387 5.34725 7.71387 7.71418C7.71387 10.0811 9.63265 11.9999 11.9996 11.9999Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+          />
+          <path
+            d="M2.57129 20.5713V18.3087C2.6117 16.6853 4.25496 15.396 6.23945 15.4291H17.7603C19.7448 15.396 21.388 16.6853 21.4284 18.3087V20.5713"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+          />
+        </svg>
+        <div className="text-center text-[12px]">프로필</div>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import photomap from "../../public/images/photomap.png";
+import Navbar from "./_components/Navbar";
 
 export default function HomePage() {
   return (
-    <div className="w-full min-h-[750px] h-[100vh] flex flex-col items-center scroll-auto">
-      <div className="bg-white w-full flex flex-col items-center gap-5 py-5">
+    <div className="relative w-full min-h-[750px] h-[100vh] flex flex-col items-center scroll-auto">
+      <div className="bg-white w-full flex flex-col items-center gap-5 py-5 pb-[66px]">
         <div className="relative rounded-xl shadow-md w-[90%] h-[166px] tablet:h-[186px]">
           <div className="text-[16px] tablet:text-lg font-bold mt-5 ml-5">
             내 근처 공공시설 찾기
@@ -42,21 +43,21 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        <div className="bg-[#18B491] rounded-xl shadow-md w-[90%] h-[564px] tablet:h-[670px] flex flex-col items-center justify-center gap-5">
+        <div className="bg-[#18B491] rounded-xl shadow-md w-[90%] h-[564px] tablet:h-[670px] flex flex-col items-center justify-center gap-5 tablet:gap-7 p-4">
           <div>
-            <div className="text-white text-2xl font-semibold text-center mb-2">
+            <div className="text-white text-2xl tablet:text-3xl font-semibold text-center mb-2 tablet:mb-4">
               근처 체육시설을
               <br />
               간편하게 찾아보세요
             </div>
-            <div className="text-[#EEEEEE] text-[13px] text-center">
+            <div className="text-[#EEEEEE] text-[13px] tablet:text-[16px] text-center">
               카카오맵 지도 연동 시스템을 사용하여
               <br />
               쉽게 찾을 수 있습니다
             </div>
           </div>
 
-          <div className="w-full max-w-[240px]">
+          <div className="w-full max-w-[240px] tablet:max-w-[280px]">
             <Image
               src={photomap}
               alt="photomap"
@@ -69,21 +70,24 @@ export default function HomePage() {
             />
           </div>
           <div className="w-full flex flex-col justify-center items-center">
-            <div className="text-white mb-2">원하는 지역을 찾아보세요</div>
-            <button className="bg-white/15 text-white border-1 border-white rounded-2xl w-[120px] h-[36px]">
+            <div className="text-white tablet:text-lg mb-2 tablet:mb-4">
+              원하는 지역을 찾아보세요
+            </div>
+            <button className="bg-white/15 text-white border-1 border-white rounded-2xl tablet:text-lg w-[120px] tablet:w-[140px] h-[36px] tablet:h-[40px]">
               검색하기
             </button>
           </div>
         </div>
         <div className="w-[90%] flex justify-between items-center">
-          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[220px] p-2">
+          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[200px] p-2">
             출석체크
           </div>
-          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[220px] p-2">
+          <div className="font-semibold rounded-xl shadow-md w-[48%] h-[140px] tablet:h-[200px] p-2">
             운동기록
           </div>
         </div>
       </div>
+      <Navbar />
     </div>
   );
 }
