6712928377a2c1d8eed87397ae71a3355ef5dda6
kmj0973 <kmj0973@naver.com>
2025-04-30 15:30:00 +0900

    Fix: pwa error

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
-const isDev = process.env.NODE_ENV === "development";
+import withPWA from "next-pwa";
 
-import nextPwa from "next-pwa";
-
-const withPWA = nextPwa({
-  dest: "public",
-  disable: isDev,
+const nextConfig = withPWA({
+  dest: "public", // service worker와 manifest.json이 생성될 위치 설정
+  disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
   register: true,
   skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/], // 일부 캐시 제외
 });
 
-const config = {
-  reactStrictMode: true,
-};
-
-export default withPWA(config);
+export default nextConfig;
