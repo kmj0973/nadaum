bd6cc58dca62ea533460279d441547e266df41be
kmj0973 <kmj0973@naver.com>
2025-04-30 17:35:31 +0900

    Feat: diet page styling

--- src/app/diet/_components/Answer.tsx
+++ src/app/diet/_components/Answer.tsx
@@ -27,7 +27,7 @@ export default function Answer() {
 
   console.log(content);
   return (
-    <div className="w-[90%] min-h-[650px] flex flex-col justify-center items-center py-5 overflow-y-auto flex-1 gap-5">
+    <div className="w-[90%] min-h-[650px] flex flex-col justify-center items-center py-5 flex-1 gap-5">
       {content === undefined ? (
         <div className="text-[#b7b7b7]">아직 식단을 만들지 않았어요</div>
       ) : (
@@ -35,6 +35,12 @@ export default function Answer() {
           <EachDietCard time={content[0]} content={content[1]} />
           <EachDietCard time={content[2]} content={content[3]} />
           <EachDietCard time={content[4]} content={content[5]} />
+          <div className="text-[#676767] text-sm tablet:text-lg flex items-center gap-2">
+            총 칼로리{" "}
+            <span className="font-extrabold text-3xl tablet:text-4xl text-[#18B491]">
+              {content[7].slice(3)}
+            </span>
+          </div>
         </>
       )}
     </div>
