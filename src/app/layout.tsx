2c697d376d28e5746c5ed3115646c8a30373c5f5
kmj0973 <kmj0973@naver.com>
2025-05-07 15:10:12 +0900

    Fix: iphone icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/basicProfile.png" />
+        <link rel="apple-touch-icon" href="/icons/basicProfile.jpg" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
